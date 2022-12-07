d12672eaa56f9f34c5498cdec1225859312085bf
jovi <kwong.jovi@gmail.com>
2022-12-07 22:51:22 +0000

    feat(App; TodoList): add useState in App. Pass prop to TodoList. Add new divs to TodoList

--- example-project-basic/src/App.js
+++ example-project-basic/src/App.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
+  // Object destructuring
+  const [todos, setTodos] = useState(["Todo 1", "Todo 2"]);
   return (
     <>
-      <TodoList />;
+      <TodoList todoList={todos} />
       <input type="text" />
       <button>Add</button>
       <button>Clear</button>
-      <div>Remaining todos:</div>
+      <div>Remaining todos: {todos.length}</div>
     </>
   );
 }
--- example-project-basic/src/TodoList.js
+++ example-project-basic/src/TodoList.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function TodoList() {
-  return <div>World's Best Todo List</div>;
+export default function TodoList({ todoList }) {
+  return (
+    <>
+      <div>World's Best Ever Todo List</div>
+      <div>
+        {todoList[0]}
+        <br></br>
+        {todoList[1]}
+      </div>
+    </>
+  );
 }
