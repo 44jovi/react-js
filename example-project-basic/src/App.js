ec99cef16e8974b318ecbe09d3c980d3053bcb3e
jovi <kwong.jovi@gmail.com>
2022-12-08 23:32:52 +0000

    feat(App): add useEffect functions to add todos to list and local storage

--- example-project-basic/src/App.js
+++ example-project-basic/src/App.js
@@ -1,15 +1,26 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
 
+const LOCAL_STORAGE_KEY = "todoApp.todos";
+
 function App() {
   // Object destructuring
-  const [todos, setTodos] = useState([
-    { id: 1, name: "Example 1", done: true },
-    { id: 2, name: "Example 2", done: false },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const todoName = useRef();
+
+  // Empty dependencies array -> only run once when app is launched
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    console.log("Local Storage Key: " + LOCAL_STORAGE_KEY);
+    console.log("savedTodos: " + savedTodos);
+    if (savedTodos) setTodos(savedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(event) {
     const name = todoName.current.value;
@@ -17,7 +28,7 @@ function App() {
     if (name === "") return;
 
     setTodos((previousTodos) => {
-      return [...previousTodos, { id: uuidv4(), name: name, done: true }];
+      return [...previousTodos, { id: uuidv4(), name: name, done: false }];
     });
 
     // Reset input box to empty
