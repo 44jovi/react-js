df0383b9dd9ce44e1f9b85791108d3af44d736cc
jovi <kwong.jovi@gmail.com>
2022-12-08 22:05:08 +0000

    feat(App): add onclick to 'Add' to target input value

--- example-project-basic/src/App.js
+++ example-project-basic/src/App.js
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
   // Object destructuring
-  const [todos, setTodos] = useState([{ id: 1, name: "Todo 1", done: false }]);
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Example 1", done: true },
+    { id: 2, name: "Example 2", done: false },
+  ]);
+
+  const todoName = useRef();
+
+  function addTodo(event) {
+    const name = todoName.current.value;
+    // Avoid adding empty todos
+    if (name === "") return;
+
+    // Implement setTodos here
+
+    // Reset input box to empty
+    todoName.current.value = null;
+  }
+
   return (
     <>
+      <div>World's Best Ever Todo List</div>
+      <hr></hr>
       <TodoList todoList={todos} />
-      <input type="text" />
-      <button>Add</button>
+      <input ref={todoName} type="text" />
+      <button onClick={addTodo}>Add</button>
       <button>Clear</button>
       <div>Remaining todos: {todos.length}</div>
     </>
