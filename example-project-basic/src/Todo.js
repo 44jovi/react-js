fdadc612579b795f24efaa25013fb02af6cd77e1
jovi <kwong.jovi@gmail.com>
2022-12-08 22:00:08 +0000

    feat(Todo): add checkbox and return todo name within label tag

--- example-project-basic/src/Todo.js
+++ example-project-basic/src/Todo.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Todo({ todo }) {
-  return <div>- {todo.name}</div>;
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={todo.done}></input>
+        {todo.name}
+      </label>
+    </div>
+  );
 }
