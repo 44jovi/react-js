6ab4153ff2e85ebbf071fb3e588bcb492810edd5
jovi <kwong.jovi@gmail.com>
2022-12-08 11:01:22 +0000

    feat(TodoList): change the returned Todo key to its new id property

--- example-project-basic/src/TodoList.js
+++ example-project-basic/src/TodoList.js
@@ -5,9 +5,10 @@ export default function TodoList({ todoList }) {
   return (
     <>
       <div>World's Best Ever Todo List</div>
+      <hr></hr>
       <div>
         {todoList.map((todo) => {
-          return <Todo key={todo} todo={todo} />;
+          return <Todo key={todo.id} todo={todo} />;
         })}
       </div>
     </>
