fe905984d956b5092d53fafb3d3e3623037df9b4
jovi <kwong.jovi@gmail.com>
2022-12-08 10:29:27 +0000

    feat(Todo; TodoList): add Todo. Return Todo component in TodoList div

--- example-project-basic/src/TodoList.js
+++ example-project-basic/src/TodoList.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Todo from "./Todo";
 
 export default function TodoList({ todoList }) {
   return (
     <>
       <div>World's Best Ever Todo List</div>
       <div>
-        {todoList[0]}
-        <br></br>
-        {todoList[1]}
+        {todoList.map((todo) => {
+          return <Todo key={todo} todo={todo} />;
+        })}
       </div>
     </>
   );
