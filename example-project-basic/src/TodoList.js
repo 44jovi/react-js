7e3f07461f2b8c5f9224571c5d77eb5d40c31113
jovi <kwong.jovi@gmail.com>
2022-12-08 12:43:42 +0000

    amend(TodoList): remove title div

--- example-project-basic/src/TodoList.js
+++ example-project-basic/src/TodoList.js
@@ -4,8 +4,6 @@ import Todo from "./Todo";
 export default function TodoList({ todoList }) {
   return (
     <>
-      <div>World's Best Ever Todo List</div>
-      <hr></hr>
       <div>
         {todoList.map((todo) => {
           return <Todo key={todo.id} todo={todo} />;
