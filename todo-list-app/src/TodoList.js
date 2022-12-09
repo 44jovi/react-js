655d8beb0a38688d0de97f89ce0cd2a90ac4adec
jovi <kwong.jovi@gmail.com>
2022-12-09 12:16:05 +0000

    feat(TodoList): inject toggleTodoStatus func. Pass to Todo component

--- todo-list-app/src/TodoList.js
+++ todo-list-app/src/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Todo from "./Todo";
 
-export default function TodoList({ todoList }) {
+export default function TodoList({ todoList, toggleTodoStatus }) {
   return (
     <>
       <div>
         {todoList.map((todo) => {
-          return <Todo key={todo.id} todo={todo} />;
+          return (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              toggleTodoStatus={toggleTodoStatus}
+            />
+          );
         })}
       </div>
     </>
