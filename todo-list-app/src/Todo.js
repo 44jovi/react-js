2648b1372224f1523fd163b85ff3ce9d845807b4
jovi <kwong.jovi@gmail.com>
2022-12-09 12:18:36 +0000

    feat(Todo): inject toggleTodoStatus func with an onChange trigger

--- todo-list-app/src/Todo.js
+++ todo-list-app/src/Todo.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleTodoStatus }) {
+  function todoCheckboxClick() {
+    toggleTodoStatus(todo.id);
+  }
+
   return (
     <div>
       <label>
-        <input type="checkbox" checked={todo.done}></input>
+        <input
+          type="checkbox"
+          checked={todo.done}
+          onChange={todoCheckboxClick}
+        ></input>
         {todo.name}
       </label>
     </div>
